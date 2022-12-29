ee4fcb299b4029bb800c5e218f12ad41dd3ae9c6
Remics89 <Remics89@gmail.com>
2022-12-28 23:52:54 -0800

    added router to final render

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client"
 import { BrowserRouter as Router, Switch, Link, Route, useHistory } from "react-router-dom";
+import { Header } from "./components";
 
 const App = () => {
     return (
-        <div className="container">
+        <div className="main">
+            <Header />
             <h1>Fitness Tracker</h1>
         </div>
     );
@@ -13,4 +15,9 @@ const App = () => {
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App />)
+root.render(
+    <Router>
+        <App />
+    </Router>
+
+)
