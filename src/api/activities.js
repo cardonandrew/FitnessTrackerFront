13dde9eafba6da48b8d6f6f1d99130358a341847
Remics89 <Remics89@gmail.com>
2022-12-29 18:55:20 -0800

    feat: built remaining api functions for activities

--- src/api/activities.js
+++ src/api/activities.js
@@ -8,14 +8,67 @@ export async function allActivities () {
                 'Content-Type': 'application/json'
             }
         })
-        const result = response.json();
-        return result
+        const result = await response.json();
+        return result;
 
     } catch (error) {
-        console.error("Could not retrieve Activities", error)
+        console.error("Could not retrieve Activities.", error)
     }
 }
 
 export async function newActivity (actName, actDesc) {
-    
+    try {
+        const response = await fetch(`${BASEURL}/activities`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${TokenString}`
+            },
+            body: JSON.stringify({
+                name: actName,
+                description: actDesc
+            })
+        })
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        console.error("Could not create new activity.", error)
+    }
+}
+
+export async function updateActivity (actId, actName, actDesc) {
+    try {
+        const response = await fetch(`${BASEURL}/activities/${actId}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${TokenString}`
+            },
+            body: JSON.stringify({
+                name: actName,
+                description: actDesc
+            })
+        })
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error("Could not update activity.", error)
+    }
+}
+
+export async function pubRoutinesByActivity (actId) {
+    try {
+        const response = await fetch(`${BASEURL}/activities/${actId}/routines`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error("Could not retrieve public routines by activity.", error)
+    }
 }
