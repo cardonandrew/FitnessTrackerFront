3324910b0e4e9865eca40f1f65577787f3d4858a
Jaycee Xu <jaycee4845@gmail.com>
2022-12-29 20:26:20 -0600

    footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function footer () {
+    return (
+        <div className="footerLogo">
+            <div className="fullstackLogo">
+                <img src="https://s3.us-east-1.amazonaws.com/fsa2-assets/assets/Logos/fullstack-academy-logo-full-color-rgb.jpg" alt="Fullstack Logo" />
+            </div>
+        
+            <div className="githubLogo">
+                <img src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="Github Logo" />
+            </div>
+        </div>
+    );
+}
+
+export default footer;
