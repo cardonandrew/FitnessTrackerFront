86c63bed807477536021361af35da4382d61dae5
Remics89 <Remics89@gmail.com>
2022-12-29 19:32:21 -0800

    dropped export default statement for routines

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,6 @@ const BASEURL = "https://fitnesstrac-kr.herokuapp.com/api";
 // API ENDPOINTS: http://fitnesstrac-kr.herokuapp.com/api/some-endpoint
 
 export { default as AuthorizeUser } from "./users";
-export { default as Routines } from "./routines";
 export { default as Routine_Activities } from "./routine_activities";
 
 export default BASEURL;
