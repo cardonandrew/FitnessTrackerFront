d32dd721de57b6d960b3997e070774a89a20e194
Remics89 <Remics89@gmail.com>
2022-12-29 18:30:51 -0800

    feat: getting all activities

--- src/api/activities.js
+++ src/api/activities.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import BASEURL from './api';
 
-const Activities = () => {
+export async function allActivities () {
+    try {
+        const response = await fetch(`${BASEURL}/activities`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const result = response.json();
+        return result
 
+    } catch (error) {
+        console.error("Could not retrieve Activities", error)
+    }
 }
 
-export default Activities;
+export async function newActivity (actName, actDesc) {
+    
+}
