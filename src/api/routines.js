32583811c4b3da2da10d95e9a336b6819bcee6a8
Remics89 <Remics89@gmail.com>
2022-12-29 19:00:31 -0800

    feat: get all routines

--- src/api/routines.js
+++ src/api/routines.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import BASEURL from './api';
 
-const Routines = () => {
-
+export async function allRoutines () {
+    try {
+        const response = await fetch(`${BASEURL}/routines`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error("Could not retrieve all routines.", error)
+    }
 }
-
-export default Routines;
