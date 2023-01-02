ab9f2ceabb6dd384f4f0f69d91d788b763e0f2e2
Remics89 <Remics89@gmail.com>
2023-01-01 19:46:19 -0800

    refactor: small changes to import/export statements and footer component

--- src/components/routines.js
+++ src/components/routines.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { allRoutines } from "../api";
+import { allRoutines } from "../api/api";
 
 const Routines = () => {
     const [routines, setRoutines] = useState([]);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom";
-import { getCurrentUser } from "./api/users";
-import { Header, Home, Routines, Dashboard, AuthorizeUser, Activities } from "./components";
+import { getCurrentUser } from "./api/api";
+import { Header, Home, Routines, Dashboard, AuthorizeUser, Activities, Footer } from "./components";
 
 
 const App = () => {
@@ -47,10 +47,10 @@ const App = () => {
                 <Route exact path="/routines" element={<Routines tokenString={tokenString}/>} />
                 <Route exact path="/account/dash" element={<Dashboard tokenString={tokenString}/>} />
                 <Route exact path="/account/:action" element={<AuthorizeUser setTokenString={setTokenString} tokenString={tokenString} />} />
-                <Route path="/activities" element={<Activities tokenString={tokenString}/>} /> 
+                <Route path="/activities" element={<Activities tokenString={tokenString}/>} />
             </Routes>
 
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 };
