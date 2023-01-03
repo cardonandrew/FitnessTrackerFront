ab11cd54b5a63a813720cafa59910f5af7a6fb70
Remics89 <Remics89@gmail.com>
2023-01-02 18:54:14 -0800

    dropped dashboard component

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as Header } from "./header";
 export { default as Home } from "./home";
 export { default as Routines } from "./routines";
 export { default as Activities } from "./activities";
-export { default as Dashboard } from "./account";
 export { default as AuthorizeUser } from "./auth";
 export { default as Activity } from "./activity";
 export { default as Popup } from "./popup"
